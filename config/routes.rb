436dd859f70a91e42b221af5078b04e86d063daf
maybethee <jozalamun@gmail.com>
2024-03-16 11:36:16 -0400

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "articles#index"
 
   resources :articles do
-    resources :commments
+    resources :comments
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
